d7f091323a0deb46a8fc3326bd5c65902a67cb73
Sebastian Hoß <sebastian.hoss@cronn.de>
2020-08-25 13:49:28 +0200

    clarify build tooling support

---
+++ src/test/java/com/example/BuildToolSupport.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import io.cucumber.junit.platform.engine.Cucumber;
+
+/**
+ * Gradle does not support JUnit platform discovery selectors yet. This class helps Gradle to discover all tests.
+ *
+ * In order to run scenarios, use 'gradle test'. Executing this class in your IDE won't work.
+ *
+ * @see <a href="https://github.com/gradle/gradle/issues/4773">Gradle 4773</a>
+ */
+@Cucumber
+public class BuildToolSupport {
+}
--- src/test/java/com/example/RunCucumberTest.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-import io.cucumber.junit.platform.engine.Cucumber;
-
-@Cucumber
-public class RunCucumberTest {
-}
