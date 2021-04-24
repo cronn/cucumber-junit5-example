63d2c55c171583c1a3ae963972f642b164ca432d
Sebastian Hoß <sebhoss@pm.me>
2021-04-24 11:44:46 +0200

    document repo requirement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "cucumber-junit5-example"
 
 dependencyResolutionManagement {
     repositories {
+        // REQUIRED: All of our dependency are available in maven central
         mavenCentral()
     }
 }
