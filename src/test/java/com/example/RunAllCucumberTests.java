831c63eab9cbd5412124c1a46eb2bbf1e6dff514
Sebastian Hoß <seb@ho.de>
2022-04-08 12:24:52 +0200

    use consistent quoting

--- src/test/java/com/example/RunAllCucumberTests.java
+++ src/test/java/com/example/RunAllCucumberTests.java
@@ -9,7 +9,7 @@ import org.junit.platform.suite.api.Suite;
  * src/test/resources contains all .feature files of this project. Note that the SelectClasspathResource annotation
  * can be repeated, thus allowing us to specify multiple locations for feature files.
  *
- * The 'cucumber' engine inclusion is not strictly necessary since that is the only engine available in this project.
+ * The "cucumber" engine inclusion is not strictly necessary since that is the only engine available in this project.
  * However, if you are planning on running regular JUnit5 tests in the same project, make sure to specify the engine
  * here.
  */
